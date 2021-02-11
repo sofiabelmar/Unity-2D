6b406167bdec1b526f055dc36954b569cf52ca8e
Sofia Beltran <sofiabelmar@outlook.com>
2021-02-10 22:03:19 -0700

    enemy 2

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -9,6 +9,9 @@ public class Enemy : MonoBehaviour
     [SerializeField]
     float timeLimit;
     float timer=0;
+    float timerDelay=0;
+    [SerializeField]
+    float delay;
     [SerializeField]
     Vector2 dir = Vector2.right;
 
@@ -21,12 +24,25 @@ public class Enemy : MonoBehaviour
 
     void Update()
     {
-        transform.Translate(dir*moveSpeed*Time.deltaTime);
+        /*transform.Translate(dir*moveSpeed*Time.deltaTime);
         timer += Time.deltaTime;
         if(timer >= timeLimit){
             timer=0;
             dir.x = dir.x > 0 ? -1 : 1;
             spr.flipX = FlipSprite;
+        }*/
+
+        if(timer < timeLimit){
+            transform.Translate(dir*moveSpeed*Time.deltaTime);
+            timer += Time.deltaTime;
+        }else{
+            timerDelay += Time.deltaTime;
+            if(timerDelay>= delay){
+                 dir.x = dir.x > 0 ? -1 : 1;
+                 spr.flipX = FlipSprite;
+                timer =0;
+                timerDelay=0;
+            }
         }
     }
 
